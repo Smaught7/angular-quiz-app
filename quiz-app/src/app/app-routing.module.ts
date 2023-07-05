8dadca1041b0e42e92fd5deefb9cb38a59fdc6d0
mateusz.jama <mateusz.jama@codefusion.pl>
2023-07-05 19:12:42 +0200

    added new component, added children path

--- quiz-app/src/app/app-routing.module.ts
+++ quiz-app/src/app/app-routing.module.ts
@@ -3,11 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategorySelectionComponent } from './category-selection/category-selection.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './category-selection/question/question.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
     path: 'quiz-category',
     component: CategorySelectionComponent,
+    children: [
+      {
+        path: ':id/:category-name',
+        component: QuestionComponent,
+      },
+    ],
   },
   {
     path: 'contact',
