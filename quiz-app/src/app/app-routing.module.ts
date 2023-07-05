dcde0b83458297b07e2545704e1c5277ac5537df
mateusz.jama <mateusz.jama@codefusion.pl>
2023-07-05 18:05:04 +0200

    create paths and components

--- quiz-app/src/app/app-routing.module.ts
+++ quiz-app/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategorySelectionComponent } from './category-selection/category-selection.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/quiz-category',
+    component: CategorySelectionComponent,
+  },
+  {
+    path: 'quiz-category',
+    component: CategorySelectionComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
